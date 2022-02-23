a44347b6658e3bf3c4bddd015ef20179debbe21f
Badi-Haki Martin <jojackblack@gmail.com>
2022-02-23 13:16:31 -0500

    commit tree 984015c0d6886648b0ce8497153f129c706e6a81

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+let distance;
+let destinationCity;
+let response;
+let tip;
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance < 400){
+    return 'This one is on me!'
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance >= 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destinationCity){
   // Write your code here!
+
+  destinationCity === 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.' ;
+
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
